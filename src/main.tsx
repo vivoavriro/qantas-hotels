60ab321bbacdd1714f2e4b919efbd96f493ea510
Parisa Esmaeil <parisa.esmaeil@qantas.com.au>
2025-02-03 20:38:25 +1100

    Add CSS reset for consistent cross-browser styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import './assets/styles/_reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
