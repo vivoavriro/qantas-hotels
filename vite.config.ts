2d6f0bffed1dafbd39f161d8a14754b15e129535
Parisa Esmaeil <parisa.esmaeil@qantas.com.au>
2025-02-03 20:30:50 +1100

    initializing the project with dependecies

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['**/*.styles.tsx', '**/*.types.ts', '**/*.index.ts'],
+    },
+  },
+});
